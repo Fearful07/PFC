aa08262cd402c9e73f090195950ade279d8a669a
Bois Johann <jbois@gaming.tech>
2022-11-08 09:36:44 +0100

    update2

--- fonction.py
+++ fonction.py
@@ -14,13 +14,13 @@ print(multiply(3,5))
 def divide (x,y):
     #si y == 0
     if y == 0:
-        #alors on afficher le message pas possible 
+        #alors on afficher le message pas possible
         print("Il faut un y superieur à 0")
         #et renvoyer rien
-        return 
+        return
     #Sinon on retourne x/y
     return x/y
-   
+
 print(divide(3,0))
 
 def modulo (x,y):
@@ -31,20 +31,20 @@ print(modulo(3,5))
 
 def revenuSeconde(salaireHeure, heureJourOuvrable, jourOuvrable):
     # je calcul le salaire annuel
-    salairAnnuel = salaireHeure * heureJourOuvrable * jourOuvrable  
+    salairAnnuel = salaireHeure * heureJourOuvrable * jourOuvrable
     # je calcul le nombre de seconde par An
-    nbSecondeParAn = 365 * 24 * 60 *60 
-    # Je divise mon salaire par le nombre de secondes dans une année 
+    nbSecondeParAn = 365 * 24 * 60 *60
+    # Je divise mon salaire par le nombre de secondes dans une année
     return salairAnnuel / nbSecondeParAn
     #return(jourOuvrable * heureJourOuvrable * salaireHeure) / (365*24*60*60)
 print(revenuSeconde(2000,17,303))
 
 def salaireNet(prc,salaireBrut):
-    #mise en pourcentage 
+    #mise en pourcentage
     percent = prc/100
     # calcul de la reduction à appliqué
-    redu = percent*salaireBrut 
-    # on soustrait le salaire brut au salaire 
+    redu = percent*salaireBrut
+    # on soustrait le salaire brut au salaire
     return salaireBrut - redu
 print(salaireNet(23,2000))
 
@@ -53,6 +53,8 @@ def withdrawFees(total, percent):
     fees = total * (percent / 100)
     # Je retourne mon total sans les taxes
     return total - fees
+
+#------------------Exercice 2: Les conditions si sinon, sinon si-------------------
 
 def calculSalaireNet(salaireBrut, public):
     #Si j'occupe un poste de la fonction publique
@@ -76,3 +78,13 @@ elif nbPersonne == 5:
     tuRentre()
 else:
     tuRentrePas()
+
+#------------------Exercice 3 : Les BOUCLES-------------------
+# tour = 0
+# # Tant que je ne suis pas au tour 5
+# while tour != 5 :
+# # Appeler la fonction JouerUnTour
+#     JouerUnTour()
+# # J'effectue l'action de passer un tour
+#     tour = tour + 1
+
