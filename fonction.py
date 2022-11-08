5e80bee17cd25cc97684cb6c7ff00d9e566b0707
Bois Johann <jbois@gaming.tech>
2022-11-08 08:23:12 +0100

    test

--- fonction.py
+++ fonction.py
@@ -1,27 +1,34 @@
 def add (x,y):
     return x+y
-print(3,5)
+print(add(3,5))
 
 def sub (x,y):
     return x-y
-print(3,5)
+print(sub(3,5))
 
 def multiply (x,y):
     return x*y
-print(3,5)
+print(multiply(3,5))
 
 def divide (x,y):
     return x/y
-print(3,5)
+print(divide(3,5))
 
 def modulo (x,y):
     return x%y
-print(3,5)
+print(modulo(3,5))
 
 def revenuSeconde(salaireHeure, heureJourOuvrable, jourOuvrable):
-    return(jourOuvrable * heureJourOuvrable) * (salaireHeure)/365/24/60/60
+    # je calcul le salaire annuel
+    salairAnnuel = salaireHeure * heureJourOuvrable * jourOuvrable  
+    # je calcul le nombre de seconde par An
+    nbSecondeParAn = 365 * 24 * 60 *60 
+    # Je divise mon salaire par le nombre de secondes dans une année 
+    return salairAnnuel / nbSecondeParAn
+    #return(jourOuvrable * heureJourOuvrable * salaireHeure) / (365*24*60*60)
 print(revenuSeconde(2000,17,303))
 
 def salaireNet(prc,salaireBrut):
-    return salaireBrut - (prc/100 * salaireBrut)
+    pourcent = prc/100 #mise en pourcentage 
+    return salaireBrut - (pourcent * salaireBrut)
 print(salaireNet(25,2000))
