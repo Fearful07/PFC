0e65232c1c600edf3b938981035f25ca48cf4ff9
Johann Bois <jbois@gaming.tech>
2022-11-09 15:30:34 +0100

    aaaaaaa

--- PFC.py
+++ PFC.py
@@ -5,26 +5,37 @@ On admet une fonction random qui renvoie un chiffre aléatoire entre un min et u
 On ademet une fonction input qui recupere l'entree d'un joueur
 
 Definir une fonction pierreFeuilleCiseau sans parametre
-    Assigne à la variable choixJoueur le retour de la fonction input("Pierre, Feuille, Ciseau ? ")
-    Tant que le choixJoueur n'est pas egale a l'un des choix de la liste
-        reappeler la fonction input("Rentrer le mot correct")
-    definir une liste choice qui comprend Pierre, Feuille et Ciseau
-    Assigne à la variable choixBot la correspondance du retour de random(0,2) dans la liste choixPossible
-    si le choixJoueur est pierre et le choixBot est ciseau 
-    ou le choixJoueur est feuille et le choixBot est pierre 
-    ou le choixJoueur est ciseau et le choixBot est feuille
-        alors
-        affiche gagner
-    sinon si le choixJoueur est egal au choixBot
-        alors 
-        affiche égalité
+    Assigne à la variable bestOff le retour de l'execution de la fonction input avec en parametre le message("Voulez-vous faire un BO1, BO3 ou BO5")
+    initialiser la variable scoreJoueur a 0
+    initialiser la variable scoreBot a 0
+    Tant que le scoreJoueur n'est pas égale à 2 et que le scoreBot n'est pas égale a 2
+        Alors
+        Assigne à la variable choixJoueur le retour de l'execution de la fonction input avec en parametre le message("Pierre, Feuille, Ciseau ? ")
+        Tant que le choixJoueur n'est pas égale a "Pierre" n'est pas égale a "Feuille" n'est pas égale a "Ciseau"
+            reappeler la fonction input("Rentrer le mot correct")
+        definir une liste choice qui comprend Pierre, Feuille et Ciseau
+        Assigne à la variable choixBot la correspondance du retour de l'execution de random avec comme parametre(0,2) dans la liste choixPossible
+        si le choixJoueur est égale a pierre et le choixBot est égale a ciseau 
+        ou le choixJoueur est égale a feuille et le choixBot est égale a pierre 
+        ou le choixJoueur est égale a ciseau et le choixBot est égale a feuille
+            alors
+            on ajoute 1 a la variable scoreJoueur
+            affiche Gagner le score est : avec le (scoreJoueur - le scoreBot)
+        sinon si le choixJoueur est egal au choixBot
+            alors 
+            affiche égalité
+        sinon
+            alors
+            on ajoute 1 a la variable scoreBot 
+            affiche Perdu le score est : avec le (scoreJoueur - le scoreBot)
+    si le scoreJoueur est 2
+        alors afficher Victoire, Fin de la partie
     sinon
-        alors
-        affiche perdu
-    assigne a la variable restart le retour de input("Voulez vous recommencer ? ")
-    si le restart est égal a oui
-        alors
-        on réexécute la fonction pierreFeuilleCiseau()
+        afficher Défaite, Fin de la partie
+            
+    assigne a la variable restart le retour de l'execution de la fonction input avec en parametre le message("Voulez vous recommencer ? ")
+    si le restart est égal a "oui"
+        alors on réexécute la fonction pierreFeuilleCiseau()
 FIN
 """
 
--- pfc_code.py
+++ pfc_code.py
@@ -1,20 +1,29 @@
 import random
 
 def pierreFeuilleCiseau():
-    choixJoueur = input("Pierre, Feuille, Ciseau ? ")
-    while choixJoueur != "Pierre" and choixJoueur != "Feuille" and choixJoueur != "Ciseau":
-        choixJoueur = input("Rentrer le mot correct")
-    choice = ["Pierre", "Feuille", "Ciseau"]
-    choixBot = choice[random.randint(0,2)]
-    if (choixJoueur == "Pierre" and choixBot == "Ciseau") or (choixJoueur == "Feuille" and choixBot == "Pierre") or (choixJoueur == "Ciseau" and choixBot == "Feuille"):
-        print("Victoire")  
-    elif choixJoueur == choixBot:
-        print("égalité")  
+    scoreJoueur = 0
+    scoreBot = 0
+    while scoreJoueur != 2 and scoreBot != 2:
+        choixJoueur = input("Pierre, Feuille, Ciseau ? ")
+        while choixJoueur != "Pierre" and choixJoueur != "Feuille" and choixJoueur != "Ciseau":
+            choixJoueur = input("Rentrer le mot correct")
+        choice = ["Pierre", "Feuille", "Ciseau"]
+        choixBot = choice[random.randint(0,2)]
+        if (choixJoueur == "Pierre" and choixBot == "Ciseau") or (choixJoueur == "Feuille" and choixBot == "Pierre") or (choixJoueur == "Ciseau" and choixBot == "Feuille"):
+            scoreJoueur = scoreJoueur + 1
+            print(f"Gagné le score est:  {scoreJoueur} - {scoreBot}")
+        elif choixJoueur == choixBot:
+            print("égalité")  
+        else:
+            scoreBot = scoreBot + 1
+            print(f"Perdu le score est:  {scoreJoueur} - {scoreBot}")
+    if scoreJoueur == 2:
+        print("Victoire, Fin de la partie")
     else:
-        print("Défaite")  
+        print("Défaite, Fin de la partie")
+
     restart = input("Voulez vous recommencer ? oui ou non ")
     if restart == "oui":
         pierreFeuilleCiseau()
-
 
 print(pierreFeuilleCiseau())
