92d41c545cc4189c803203c0c524e770f72048a0
Johann Bois <jbois@gaming.tech>
2022-11-10 18:55:41 +0100

    PFC_Final

--- pfc_code.py
+++ pfc_code.py
@@ -1,12 +1,16 @@
 import random
 
 def pierreFeuilleCiseau():
+    bestOff = int(input("Le premier à combien ? "))
+    print(bestOff)
     scoreJoueur = 0
     scoreBot = 0
-    while scoreJoueur != 2 and scoreBot != 2:
+    nbRound = 0
+    while scoreJoueur != bestOff and scoreBot != bestOff:
         choixJoueur = input("Pierre, Feuille, Ciseau ? ")
+        nbRound += 1
         while choixJoueur != "Pierre" and choixJoueur != "Feuille" and choixJoueur != "Ciseau":
-            choixJoueur = input("Rentrer le mot correct")
+            choixJoueur = input("Rentrer le mot correct ")
         choice = ["Pierre", "Feuille", "Ciseau"]
         choixBot = choice[random.randint(0,2)]
         if (choixJoueur == "Pierre" and choixBot == "Ciseau") or (choixJoueur == "Feuille" and choixBot == "Pierre") or (choixJoueur == "Ciseau" and choixBot == "Feuille"):
@@ -17,10 +21,10 @@ def pierreFeuilleCiseau():
         else:
             scoreBot = scoreBot + 1
             print(f"Perdu le score est:  {scoreJoueur} - {scoreBot}")
-    if scoreJoueur == 2:
-        print("Victoire, Fin de la partie")
+    if scoreJoueur == bestOff:
+        print("Victoire, Fin de la partie", "Avec", nbRound, "joué")
     else:
-        print("Défaite, Fin de la partie")
+        print("Défaite, Fin de la partie", "Avec", nbRound, "round joué")
 
     restart = input("Voulez vous recommencer ? oui ou non ")
     if restart == "oui":
